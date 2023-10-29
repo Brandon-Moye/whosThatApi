71ffde81f235832c76dfa4678f2d95b4e0861b29
Brandon Moye <brandon.m.moye@gmail.com>
2023-10-29 16:25:08 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cheerio = require('cheerio')
+const fetch = require('node-fetch')
+const app = express()
+const port = 5353
+
+//Variables
+const baseUrl = (pokemon) => `https://pokemondb.net/pokedex/${pokemon}`
+
+//MIDDLEWARE
+app.use(require('cors')())
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.status(200).send({message: 'Thank you for trying our API'})
+})
+
+app.get('/api/whosThatApi/Pokemon', async (req, res) => {
+    const { pokemon } = req.query
+    console.log(`Pokemon Name: ${pokemon}`)
+    if(!pokemon) {
+        return res.sendStatus(403)
+    }
+
+
+    try {
+        const pokedexEntryUrl = baseUrl(pokemon)
+        const pokemonRes = await fetch(pokedexEntryUrl)
+        const pokemonDataInText = await pokemonRes.text()
+        console.log(pokemonDataInText)
+        const $ = cheerio.load(pokemonDataInText)
+        console.log($.html)
+        res.sendStatus(200)
+    } catch (err) {
+        console.log(`Looks like Team Rocket is blasting off again! ${err}`)
+        res.sendStatus(500)
+    }
+})
+app.listen(port, () => console.log(`Lets research some Pokemon! ${port}`))
